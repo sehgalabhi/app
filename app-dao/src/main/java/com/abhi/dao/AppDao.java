a240428f9bc2849d551badb17aa588fac44059bb
Abhishek Sehgal <abhi@localhost.localdomain>
2018-06-24 16:51:08 +0530

    Version1

---
+++ app-dao/src/main/java/com/abhi/dao/AppDao.java
@@ -0,0 +1,17 @@
+package com.abhi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by abhi on 24/6/18.
+ */
+public interface AppDao {
+
+
+    //TODO Add Logger
+    public Connection getConnection() throws SQLException ;
+
+
+}
