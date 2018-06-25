8aaddf9f9b6cbeb85466fdcdd3e1385806699a86
Abhishek Sehgal <asehgal@rambus.com>
2018-06-25 12:01:34 +0200

    Version 2 spring orm

--- app-dao/src/test/java/com/abhi/dao/AppDaoTestNativeHibernate.java
+++ app-dao/src/test/java/com/abhi/dao/AppDaoTestNativeHibernate.java
@@ -23,12 +23,13 @@ import java.util.Date;
  * Created by abhi on 24/6/18.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath*:/applicationContext.xml")
+@ContextConfiguration(locations = "classpath*:/app-dao-module.xml")
 public class AppDaoTestNativeHibernate {
 
     @Inject
     private AppDao appDao;
 
+    @Inject
     private SessionFactory sessionFactory;
 
     @Before
@@ -39,37 +40,24 @@ public class AppDaoTestNativeHibernate {
 
     @After
     public void tearDown() throws Exception {
-        if (sessionFactory != null) {
-            sessionFactory.close();
-        }
+
     }
 
     @Test
     public void testHibernate() {
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Event("Our very first event!", new Date()));
+        session.save(new Event("A follow up event", new Date()));
+        session.getTransaction().commit();
+        session.close();
 
-        try (SessionFactory sessionFactoryTmp = new MetadataSources(registry).buildMetadata().buildSessionFactory()) {
-            sessionFactory = sessionFactoryTmp;
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.save(new Event("Our very first event!", new Date()));
-            session.save(new Event("A follow up event", new Date()));
-            session.getTransaction().commit();
-            session.close();
-
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            Query<Event> namedQuery = session.createQuery("from Event", Event.class);
-            namedQuery.list().forEach(t -> System.out.println(t.getId() + "" + t.getTitle() + t.getDate()));
-            session.getTransaction().commit();
-            session.close();
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<Event> namedQuery = session.createQuery("from Event", Event.class);
+        namedQuery.list().forEach(t -> System.out.println(t.getId() + "" + t.getTitle() + t.getDate()));
+        session.getTransaction().commit();
+        session.close();
 
     }
 
